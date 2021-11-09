2e118f6b94dfca4adf793892b3c9b1b3ba6a1b72
SayleMz <41290157+SayleMz@users.noreply.github.com>
2021-11-09 21:26:12 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,37 +4,37 @@
 #include <iomanip>  /* pour la fonction setw()        */
 using namespace std;
 
-bool estPremier(int nombre)
+bool estPremier(int nombre) /*fonction qui permet de vérifier si un nombre est premier*/
 {
-    for (int i = 1; i < nombre; i++)
+    for (int i = 1; i < nombre; i++) /*On vérifie la division du nombre par i, soit tout les entiers allant de 1 au nombre*/
     {
         if (nombre % i == 0
-            && i != 1) return false;
+            && i != 1) return false; /*On renvoie 0 car le nombre est divisible par i*/
     }
-    return true;
+    return true; /*On renvoie 1 car le nombre n'est divisible que par 1 et lui même*/
 }
 
-int main(int argc, char *argv[])
+int main(void)
 {
     vector<int> facteurs;
-    int nombre = 0;
+    int nombre = 0; /*On initialise nos variables*/
     cout << "Choisissez un nombre a decomposer: ";
-    cin >> nombre;
-    const int ESPACE = to_string(nombre).length();
+    cin >> nombre; /*On lit le nombre*/
+    const int ESPACE = to_string(nombre).length(); /*permet de calculer le nombre de chiffre du nombre choisis*/
     while (nombre != 1)
     {
         for (int i = 2; i <= nombre; i++)
         {
-            if (estPremier(i) && nombre % i == 0)
+            if (estPremier(i) && nombre % i == 0) /*On teste si i est premier et si le nombre est divisible par i*/
             {
-                cout << setw(ESPACE) << nombre << " | " << i << endl;
-                nombre = nombre / i;
-                facteurs.push_back(i);
+                cout << setw(ESPACE) << nombre << " | " << i << endl; /*On affiche le nombre et son diviseur*/
+                nombre = nombre / i; /*On divise le nombre*/
+                facteurs.push_back(i); /*on ajoute le diviseur à la liste des facteurs*/
             }
         }
     }
-    cout << setw(ESPACE) << 1 << " |" << endl << "= ";
+    cout << setw(ESPACE) << 1 << " |" << endl << "= "; /*On affiche le dernier diviseur, en l'occurence 1*/
     for (size_t i = 0; i < facteurs.size(); i++)
-        cout << facteurs[i] << (char)(i < facteurs.size()-1 ? '*' : ' '); 
+        cout << facteurs[i] << (char)(i < facteurs.size()-1 ? '*' : ' '); /*On affiche la liste des facteurs sous forme de multiple = f1*f2... */
     return EXIT_SUCCESS;
 }
