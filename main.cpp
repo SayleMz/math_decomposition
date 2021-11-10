371d6d4bbcc555f5399a6570ffa7979eb63d4696
SayleMz <41290157+SayleMz@users.noreply.github.com>
2021-11-10 13:59:22 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,21 @@
-#include <iostream> /* pour les flux cout et cin      */
-#include <string>   /* pour les chaînes de caractères */
-#include <vector>   /* pour les tableaux de valeurs   */
-#include <iomanip>  /* pour la fonction setw()        */
+#include <iostream>
+#include <string>  
+#include <vector>  
+#include <iomanip> 
 using namespace std;
 
-bool estPremier(int nombre)
-{
-    for (int i = 1; i < nombre; i++)
-    {
-        if (nombre % i == 0
-            && i != 1) return false;
-    }
-    return true;
-}
-
-int main(int argc, char *argv[])
+int main(void)
 {
     vector<int> facteurs;
-    int nombre = 0;
+    int nombre = 0; 
     cout << "Choisissez un nombre a decomposer: ";
-    cin >> nombre;
-    const int ESPACE = to_string(nombre).length();
+    cin >> nombre; 
+    const int ESPACE = to_string(nombre).length(); 
     while (nombre != 1)
     {
         for (int i = 2; i <= nombre; i++)
         {
-            if (estPremier(i) && nombre % i == 0)
+            if (nombre % i == 0) 
             {
                 cout << setw(ESPACE) << nombre << " | " << i << endl;
                 nombre = nombre / i;
@@ -33,8 +23,8 @@ int main(int argc, char *argv[])
             }
         }
     }
-    cout << setw(ESPACE) << 1 << " |" << endl << "= ";
+    cout << setw(ESPACE) << 1 << " |" << endl << "= "; 
     for (size_t i = 0; i < facteurs.size(); i++)
-        cout << facteurs[i] << (char)(i < facteurs.size()-1 ? '*' : ' '); 
+        cout << facteurs[i] << (char)(i < facteurs.size()-1 ? '*' : ' ');
     return EXIT_SUCCESS;
 }
